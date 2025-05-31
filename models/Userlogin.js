667a2bda71bc2f4223e0c124e0a56714d6ff4c48
revanth-1807 <revanth1807r@gmail.com>
2025-05-31 12:12:17 +0530

    Userlogin.js

--- models/Userlogin.js
+++ models/Userlogin.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   Name: { type: String, required: true },
   Email: { type: String, required: true, unique: true },
-  Password: { type: String, required: true },
-  ConfirmPassword: { type: String, required: true }
-});
+  Password: { type: String, required: true }
+}, { strict: true }); // This ignores any extra fields
 
 module.exports = mongoose.model('Userlogin', userSchema);
